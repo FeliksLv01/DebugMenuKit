cfbe25a18f86bdfed5710f08990195344b4f9c12
FeliksLv <felikslv@163.com>
2026-09-12 04:43:27 +0800

    fix(package): bundle macro targets in root package

--- Package.swift
+++ Package.swift
@@ -1,11 +1,13 @@
 // swift-tools-version: 6.0
 
+import CompilerPluginSupport
 import PackageDescription
 
 let package = Package(
     name: "DebugMenuKit",
     platforms: [
         .iOS(.v15),
+        .macOS(.v10_15),
     ],
     products: [
         .library(
@@ -14,14 +16,26 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(path: "DebugMenuKitMacros"),
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "603.0.0"),
     ],
     targets: [
+        .macro(
+            name: "DebugMenuKitMacros",
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+            ],
+            path: "DebugMenuKitMacros/Sources/DebugMenuKitMacros"
+        ),
+        .target(
+            name: "DebugMenuKitMacro",
+            dependencies: ["DebugMenuKitMacros"],
+            path: "DebugMenuKitMacros/Sources/DebugMenuKitMacro"
+        ),
         .target(
             name: "DebugMenuKit",
-            dependencies: [
-                .product(name: "DebugMenuKitMacro", package: "debugmenukitmacros"),
-            ],
+            dependencies: ["DebugMenuKitMacro"],
             path: "Sources"
         ),
         .testTarget(
